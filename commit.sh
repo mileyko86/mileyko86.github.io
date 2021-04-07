5ca3bd546fc61ea1839adf836459f7bd34bdaad5
mileyko <>
2021-04-07 19:44:07 +0000

    added new picture in ff4

---
+++ commit.sh
@@ -0,0 +1,7 @@
+git init
+git add --all
+git config --global user.name "mileyko86"
+git config --global user.email "mileyko@cps.edu"
+git commit -m "added new picture in ff4"
+git remote add origin https://github.com/mileyko86/mileyko86.github.io.git
+git push -u origin master
